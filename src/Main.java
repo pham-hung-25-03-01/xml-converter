987821243dcb25d9dae64134cbcac90c45481587
pham-hung-25-03-01 <phamhunggl721@gmail.com>
2023-04-25 11:54:38 +0700

    add lib

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package Run;
 
-import Views.MainForm;
+
+import views.MainForm;
 
 /**
  *
  * @author ASUS RG
  */
-public class run {
+public class Main {
 
     /**
      * @param args the command line arguments
---
+++ src/services/IConverter.java
@@ -0,0 +1,5 @@
+package services;
+
+public interface IConverter {
+    public String convertToXmlFile(String sourceFilePath);
+}
