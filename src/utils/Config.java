be6186df331a081da7249610c982f61dfc131657
pham-hung-25-03-01 <phamhunggl721@gmail.com>
2023-05-19 18:04:28 +0700

    add config database

--- src/utils/Config.java
+++ src/utils/Config.java
@@ -2,7 +2,10 @@ package utils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.xml.stream.XMLEventReader;
@@ -10,15 +13,24 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import org.ini4j.InvalidFileFormatException;
 import org.ini4j.Wini;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.properties.EncryptableProperties;
+import org.jasypt.salt.RandomIVGenerator;
 
 
 public class Config {
 
-    public static Wini getConfigPath() throws InvalidFileFormatException, IOException{
-        File file = new File("configs/path.ini");
-        file.getParentFile().mkdirs();
-        file.createNewFile();
-        
+    public static Wini getConfigPath() throws InvalidFileFormatException, IOException {
+        File file = new File("configs/path.ini") {{
+            getParentFile().mkdirs();
+            createNewFile();
+            if (length() == 0) {
+                FileOutputStream fos = new FileOutputStream(this);
+                fos.write("[DefaultValues]\nPATH=configs/default/values.properties\n\n[DatabaseConfig]\nPATH=configs/default/database.properties".getBytes());
+                fos.close();
+            }
+        }};
+
         return new Wini(file) {{
             setConfig(new org.ini4j.Config() {{
                 setStrictOperator(true);
@@ -34,6 +46,12 @@ public class Config {
 
     public static Properties getConfigDefaultValues() throws InvalidFileFormatException, IOException {
         String defaultValuesFilePath = getConfigPath().get("DefaultValues", "PATH");
+
+        new File(defaultValuesFilePath) {{
+            getParentFile().mkdirs();
+            createNewFile();
+        }};
+
         return new Properties() {{
             load(new FileInputStream(defaultValuesFilePath));
         }};
@@ -47,5 +65,46 @@ public class Config {
 
         return reader;
     }
-    
+
+    public static Properties getConfigDatabase() throws IOException {
+        String databaseFilePath = getConfigPath().get("DatabaseConfig", "PATH");
+
+        new File(databaseFilePath) {{
+            getParentFile().mkdirs();
+            createNewFile();
+            if (length() == 0) {
+                FileOutputStream fos = new FileOutputStream(this);
+                fos.write("HOST=\nPORT=\nNAME=\nUSER=\nPASSWORD=".getBytes());
+                fos.close();
+            }
+        }};
+
+        Properties properties = new EncryptableProperties(getEncryptor());
+        properties.load(new FileInputStream(databaseFilePath));
+
+        return properties;
+    }
+
+    public static void setConfigDatabase(HashMap<String, String> configs) throws IOException {
+        String databaseFilePath = getConfigPath().get("DatabaseConfig", "PATH");
+
+        Properties properties = getConfigDatabase();
+
+        StandardPBEStringEncryptor encryptor = getEncryptor();
+
+        for(Map.Entry<String, String> entry : configs.entrySet()) {
+            properties.put(entry.getKey(), "ENC(" + encryptor.encrypt(entry.getValue()) + ")");
+        }
+        properties.store(new FileOutputStream(databaseFilePath), null);
+    }
+
+    private static StandardPBEStringEncryptor getEncryptor() {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();     
+        encryptor.setPassword("OpenWayGroup");
+        encryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
+        encryptor.setIVGenerator(new RandomIVGenerator());
+        
+        return encryptor;
+    }
+
 }
