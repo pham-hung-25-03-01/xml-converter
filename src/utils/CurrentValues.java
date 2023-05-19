5bba8e796da10458d6ada49adb2552a75f8e0747
pham-hung-25-03-01 <phamhunggl721@gmail.com>
2023-05-19 18:03:33 +0700

    add ref default value

--- src/utils/CurrentValues.java
+++ src/utils/CurrentValues.java
@@ -8,10 +8,12 @@ public class CurrentValues {
     public static HashMap<String, String> Attributes = new HashMap<String, String>() {{
         put("TYPE", "DEFAULT");
         put("USE", "DEFAULT");
+        put("REF", "[]");
     }};
 
     public static void setDefaultAttributes() {
         Attributes.put("TYPE", "DEFAULT");
         Attributes.put("USE", "DEFAULT");
+        Attributes.put("REF", "[]");
     }
 }
